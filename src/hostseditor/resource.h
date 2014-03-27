1184257c95b8e34589a4a6ea5e4bc81922517ade
hufuman <hacker931@163.com>
2014-03-27 14:17:00 +0800

    add base source

---
+++ src/hostseditor/resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by HostsEditor.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_HOSTSEDITOR_DIALOG          102
+#define IDD_ABOUTBOX                    103
+#define IDI_HOSTSEDITOR                 107
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG_MAIN                 129
+#define IDS_ABOUTBOX                    130
+#define IDS_APP_NAME                    142
+#define IDC_LABEL_AUTHOR                1011
+#define IDC_LIST1                       1013
+#define IDC_LIST_MODES                  1013
+#define IDC_EDIT_HOSTS                  1014
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        141
+#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_CONTROL_VALUE         1015
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
